fd13d14eef04bd4b7986b1bfe923faf91b5eb646
Craig Edwards <craigedwards@brainbox.cc>
2021-04-04 17:56:21 +0100

    fix issue copying spdlogd.dll

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@ mkdir build
 cd build
 cmake -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=TRUE ..
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" test.sln
-copy /y deps\spdlog\Debug\spdlogd.dll spdlogd.dll
+copy /y deps\spdlog\Debug\spdlogd.dll Debug\spdlogd.dll
