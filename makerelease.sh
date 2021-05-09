aa4f24eb52bbb2261a6f70287722b3c0af81fc9c
brain <craigedwards@brainbox.cc>
2021-05-10 00:40:40 +0100

    working script

--- makerelease.sh
+++ makerelease.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+LASTVER=`git tag | sort -t "." -k1,1n -k2,2n -k3,3n | tail -n1`
+MINOR=`echo -n $LASTVER | sed "s/v[0-9]*\.[0-9]*\.//"`
+MAJOR=`echo -n $LASTVER | sed "s/\.[0-9]*$//"`
+NEWMINOR=$((MINOR + 1))
+NEWVER="$MAJOR.$NEWMINOR"
+echo "Building and tagging release $NEWVER"
+git tag -a "$NEWVER" -m "Release $NEWVER"
+git push
 cd /tmp
-git clone --depth=1 --recursive git@github.com:brainboxdotcc/DPP.git
+rm -rf /tmp/DPP
+git clone --depth=1 --branch "$NEWVER" --recursive git@github.com:brainboxdotcc/DPP.git
 cd /tmp/DPP
-rm -rf .git*
-
+rm -rf /tmp/DPP/.git*
+rm -rf /tmp/DPP/.circleci /tmp/DPP/.gdbargs /tmp/DPP/.vscode /tmp/DPP/makerelease.sh
+cd /tmp
+tar cvj /tmp/DPP > /tmp/DPP-Release.tar.bz2
+mkdir ~/releases
+cp /tmp/DPP-Release.tar.bz2 "~/releases/DPP-$NEWVER.tar.bz2"
