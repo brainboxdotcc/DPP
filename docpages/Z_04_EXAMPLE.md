ff2f0a24ba7344a21ef695b31fd3634f396f41bf
brain <craigedwards@brainbox.cc>
2021-05-11 00:32:50 +0100

    fix example page for slash command interactions, broken by buttons

--- docpages/Z_04_EXAMPLE.md
+++ docpages/Z_04_EXAMPLE.md
@@ -336,33 +336,34 @@ and interactions, making this process even more seamless.
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
 #include <dpp/dpp.h>
 #include <fmt/format.h>
-#include <iomanip>
 
-int main(int argc, char const *argv[])
+int main()
 {
 	dpp::cluster bot("token");
 
 	/* The interaction create event is fired when someone issues your commands */
-	bot.on_interaction_create([&bot](const dpp::interaction_create_t & cmd) {
-		/* Check which command they ran */
-		if (cmd.command.data.name == "blep") {
-			/* Fetch a parameter value from the command parameters */
-			std::string animal = std::get<std::string>(cmd.get_parameter("animal"));
-			/* Reply to the command. There is an overloaded version of this
-			 * call that accepts a dpp::message so you can send embeds.
-			 */
-			cmd.reply(dpp::ir_channel_message_with_source, fmt::format("Blep! You chose {}", animal));
+	bot.on_interaction_create([&bot](const dpp::interaction_create_t & event) {
+		if (event.command.type == dpp::it_application_command) {
+			dpp::command_interaction cmd_data = std::get<dpp::command_interaction>(event.command.data);
+			/* Check which command they ran */
+			if (cmd_data.name == "blep") {
+				/* Fetch a parameter value from the command parameters */
+				std::string animal = std::get<std::string>(event.get_parameter("animal"));
+				/* Reply to the command. There is an overloaded version of this
+				* call that accepts a dpp::message so you can send embeds.
+				*/
+				event.reply(dpp::ir_channel_message_with_source, fmt::format("Blep! You chose {}", animal));
+			}
 		}
 	});
 
 	bot.on_ready([&bot](const dpp::ready_t & event) {
-		dpp::slashcommand newcommand;
 
+		dpp::slashcommand newcommand;
 		/* Create a new global command on ready event */
-		newcommand
-			.set_name("blep")
+		newcommand.set_name("blep")
 			.set_description("Send a random adorable animal photo")
-			.set_application_id(bot.me.id);
+			.set_application_id(bot.me.id)
 			.add_option(
 				dpp::command_option(dpp::co_string, "animal", "The type of animal", true).
 					add_choice(dpp::command_option_choice("Dog", std::string("animal_dog"))).
@@ -374,8 +375,10 @@ int main(int argc, char const *argv[])
 
 		/* Register the command */
 		bot.global_command_create(newcommand);
+	});
 
 	bot.start(false);
+
 	return 0;
 }
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- src/test.cpp
+++ src/test.cpp
@@ -1,42 +1,44 @@
 #include <dpp/dpp.h>
-#include <iostream>
-#include <dpp/nlohmann/json.hpp>
-#include <dpp/message.h>
-
-using json = nlohmann::json;
+#include <fmt/format.h>
 
 int main()
 {
-	json configdocument;
-	std::ifstream configfile("../config.json");
-	configfile >> configdocument;
+	dpp::cluster bot("token");
 
-	dpp::cluster bot(configdocument["token"], dpp::i_default_intents | dpp::i_guild_members);
-
-	bot.on_log([](const dpp::log_t & event) {
-		if (event.severity > dpp::ll_trace) {
-			std::cout << event.message << "\n";
+	/* The interaction create event is fired when someone issues your commands */
+	bot.on_interaction_create([&bot](const dpp::interaction_create_t & event) {
+		if (event.command.type == dpp::it_application_command) {
+			dpp::command_interaction cmd_data = std::get<dpp::command_interaction>(event.command.data);
+			/* Check which command they ran */
+			if (cmd_data.name == "blep") {
+				/* Fetch a parameter value from the command parameters */
+				std::string animal = std::get<std::string>(event.get_parameter("animal"));
+				/* Reply to the command. There is an overloaded version of this
+				* call that accepts a dpp::message so you can send embeds.
+				*/
+				event.reply(dpp::ir_channel_message_with_source, fmt::format("Blep! You chose {}", animal));
+			}
 		}
 	});
 
-	bot.on_button_click([&bot](const dpp::button_click_t & event) {
-		event.reply(dpp::ir_channel_message_with_source, "Clicky McClickface");
-	});
+	bot.on_ready([&bot](const dpp::ready_t & event) {
 
-	bot.on_message_create([&bot](const dpp::message_create_t & event) {
-
-		if (event.msg->content == "!ping2") {
-			bot.message_create(
-				dpp::message(event.msg->channel_id, "this text has buttons").add_component(
-					dpp::component().add_component(
-						dpp::component().set_label("Click me!").
-						set_emoji("😄").
-						set_style(dpp::cos_danger).
-						set_id("myid")
-					)
+		dpp::slashcommand newcommand;
+		/* Create a new global command on ready event */
+		newcommand.set_name("blep")
+			.set_description("Send a random adorable animal photo")
+			.set_application_id(bot.me.id)
+			.add_option(
+				dpp::command_option(dpp::co_string, "animal", "The type of animal", true).
+					add_choice(dpp::command_option_choice("Dog", std::string("animal_dog"))).
+					add_choice(dpp::command_option_choice("Cat", std::string("animal_cat"))).
+					add_choice(dpp::command_option_choice("Penguin", std::string("animal_penguin")
 				)
-			);
-		}
+			)
+		);
+
+		/* Register the command */
+		bot.global_command_create(newcommand);
 	});
 
 	bot.start(false);
