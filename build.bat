97e5db8d9db43432360cd70d501978ba666e33fb
brain <craigedwards@brainbox.cc>
2021-05-11 10:26:07 +0100

    no separate fmtd copy

--- build.bat
+++ build.bat
@@ -2,7 +2,6 @@ mkdir build
 cd build
 cmake -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=TRUE-DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=../my/project/compiler-settings-toolchain.cmake  ..
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" libdpp.sln
-copy /y bin\Debug\fmtd.dll Debug\fmtd.dll
 copy /y C:\vckpg\installed\x64-windows\bin\zlib1.dll Debug\zlib1.dll
 copy /y C:\vckpg\installed\x64-windows\bin\libcrypto-1_1-x64.dll Debug\libcrypto-1_1-x64.dll
 copy /y C:\vckpg\installed\x64-windows\bin\libssl-1_1-x64.dll Debug\libssl-1_1-x64.dll
