8ca4fc6bb05ffc2d393a0410e3e786cb6ea06dad
Daniel Nery <danielnso97@gmail.com>
2021-05-17 20:24:09 -0300

    Change slashcommand json serialization

--- src/dpp/slashcommand.cpp
+++ src/dpp/slashcommand.cpp
@@ -2,7 +2,7 @@
  *
  * D++, A Lightweight C++ library for Discord
  *
- * Copyright 2021 Craig Edwards and D++ contributors 
+ * Copyright 2021 Craig Edwards and D++ contributors
  * (https://github.com/brainboxdotcc/DPP/graphs/contributors)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,84 +28,77 @@ namespace dpp {
 
 using json = nlohmann::json;
 
-slashcommand::slashcommand() : managed() 
+slashcommand::slashcommand() : managed()
 {
 }
 
 slashcommand::~slashcommand() {
 }
 
-
-
 slashcommand& slashcommand::fill_from_json(nlohmann::json* j) {
 	id = SnowflakeNotNull(j, "id");
 	return *this;
 }
 
+void to_json(json& j, const command_option_choice& choice) {
+	j["name"] = choice.name;
+	if (std::holds_alternative<uint32_t>(choice.value)) {
+		j["value"] = std::get<uint32_t>(choice.value);
+	} else if (std::holds_alternative<bool>(choice.value)) {
+		j["value"] = std::get<bool>(choice.value);
+	} else if (std::holds_alternative<snowflake>(choice.value)) {
+		j["value"] = std::to_string(std::get<uint64_t>(choice.value));
+	} else {
+		j["value"] = std::get<std::string>(choice.value);
+	}
+}
+
+void to_json(json& j, const command_option& opt) {
+	j["name"] = opt.name;
+	j["description"] = opt.description;
+	j["type"] = opt.type;
+	j["required"] = opt.required;
+
+	if (opt.options.size()) {
+		j["options"] = json();
+
+		for (const auto& opt : opt.options) {
+			json jopt = opt;
+			j["options"].push_back(jopt);
+		}
+	}
+
+	if (opt.choices.size()) {
+		j["choices"] = json();
+
+		for (const auto& choice : opt.choices) {
+			json jchoice = choice;
+			j["choices"].push_back(jchoice);
+		}
+	}
+}
+
+void to_json(json& j, const slashcommand& p) {
+	j["name"] = p.name;
+	j["description"] = p.description;
+
+	if (p.options.size()) {
+		j["options"] = json();
+
+		for (const auto& opt : p.options) {
+			json jopt = opt;
+			j["options"].push_back(jopt);
+		}
+	}
+}
+
 std::string slashcommand::build_json(bool with_id) const {
-	json j;
+	json j = *this;
+
 	if (with_id) {
 		j["id"] = std::to_string(id);
 	}
-	j["name"] = name;
-	j["description"] = description;
-	if (options.size()) {
-		j["options"] = json();
-		json& o = j["options"];
-		for (auto & opt : options) {
-			json n;
-			n["name"] = opt.name;
-			n["description"] = opt.description;
-			n["type"] = opt.type;
-			n["required"] = opt.required;
 
-			if (opt.choices.size()) {
-				n["choices"] = json();
-				json &c = n["choices"];
-				for (auto & choice : opt.choices) {
-					json t;
-					t["name"] = choice.name;
-					if (std::holds_alternative<uint32_t>(choice.value)) {
-						t["value"] = std::get<uint32_t>(choice.value);
-					} else {
-						t["value"] = std::get<std::string>(choice.value);
-					}
-					c.push_back(t);
-				}
-			}
-
-			if (opt.options.size()) {
-				n["options"] = json();
-				json &c = n["options"];
-				for (auto & subcommand : opt.options) {
-					json o;
-					o["name"] = opt.name;
-					o["description"] = opt.description;
-					o["type"] = opt.type;
-					o["required"] = opt.required;
-					if (opt.choices.size()) {
-						o["choices"] = json();
-						json &v = o["choices"];
-						for (auto & choice : opt.choices) {
-							v["name"] = choice.name;
-							if (std::holds_alternative<uint32_t>(choice.value)) {
-								v["value"] = std::get<uint32_t>(choice.value);
-							} else if (std::holds_alternative<bool>(choice.value)) {
-								v["value"] = std::get<bool>(choice.value);
-							} else if (std::holds_alternative<snowflake>(choice.value)) {
-								v["value"] = std::to_string(std::get<uint64_t>(choice.value));
-							} else {
-								v["value"] = std::get<std::string>(choice.value);
-							}
-						}
-						v.push_back(o);
-					}
-					c.push_back(n);
-				}
-			}
-			o.push_back(n);
-		}
-	}
 	return j.dump();
 }
 
@@ -249,6 +242,4 @@ std::string interaction_response::build_json() const {
 	return j.dump();
 }
 
-
 };
-
