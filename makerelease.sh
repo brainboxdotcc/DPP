b38c0662c3d7c9df68a513af352da0b706434422
brain <craigedwards@brainbox.cc>
2021-09-15 11:09:15 +0100

    update to include debug win build

--- makerelease.sh
+++ makerelease.sh
@@ -12,11 +12,19 @@ mv "./libdpp - Debian Package amd64/libdpp-$NEWVER-Linux.deb" "./assets/libdpp-$
 mv "./libdpp - Debian Package Linux x86/libdpp-$NEWVER-Linux.deb" "./assets/libdpp-$NEWVER-linux-i386.deb"
 mv "./libdpp - Debian Package ARM64/libdpp-$NEWVER-Linux.deb" "./assets/libdpp-$NEWVER-linux-rpi-arm64.deb"
 mv "./libdpp - Debian Package ARMHF/libdpp-$NEWVER-Linux.deb" "./assets/libdpp-$NEWVER-linux-rpi-armhf.deb"
-mv "./libdpp - Windows x64/libdpp-$NEWVER-win64.zip" "./assets/libdpp-$NEWVER-win64-release-vs2019.zip"
+mv "./libdpp - Windows x64-Release/libdpp-$NEWVER-win64.zip" "./assets/libdpp-$NEWVER-win64-release-vs2019.zip"
+mv "./libdpp - Windows x64-Debug/libdpp-$NEWVER-win64.zip" "./assets/libdpp-$NEWVER-win64-debug-vs2019.zip"
+
 cd assets
-mkdir -p libdpp-$NEWVER-win64/bin
-cp ../../win32/bin/*.dll libdpp-$NEWVER-win64/bin
-zip -g *.zip libdpp-$NEWVER-win64/bin/*
+
+mkdir -p libdpp-$NEWVER-win64-release/bin
+cp ../../win32/bin/*.dll libdpp-$NEWVER-win64-release/bin
+zip -g libdpp-$NEWVER-win64-release-vs2019.zip libdpp-$NEWVER-win64-release/bin/*
+
+mkdir -p libdpp-$NEWVER-win64-debug/bin
+cp ../../win32/bin/*.dll libdpp-$NEWVER-win64-debug/bin
+zip -g libdpp-$NEWVER-win64-debug-vs2019.zip libdpp-$NEWVER-win64-debug/bin/*
+
 cd ..
 echo "Create release..."
 gh release create "v$NEWVER" --draft --title "v$NEWVER release" --notes "$(git log --format="- %s" $(git show-ref | grep refs/tags | tail -n 1 | cut -d ' ' -f 1)..HEAD)" ./assets/*.zip ./assets/*.deb
