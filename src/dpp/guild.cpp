249274a58178b89204484e66d00238d71e9fb7d9
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-09-16 22:08:20 +0700

    refactor(guild): use static const for featuremap

--- src/dpp/guild.cpp
+++ src/dpp/guild.cpp
@@ -28,7 +28,7 @@
 
 using json = nlohmann::json;
 
-std::map<std::string, dpp::guild_flags> featuremap = {
+static const std::map<std::string, dpp::guild_flags> featuremap = {
 	{"INVITE_SPLASH", dpp::g_invite_splash },
 	{"VIP_REGIONS", dpp::g_vip_regions },
 	{"VANITY_URL", dpp::g_vanity_url },
