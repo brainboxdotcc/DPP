888cfee34fd54baef80159b430bdb9da73b32ec0
Axyte <rag21082004@gmail.com>
2021-09-18 23:51:00 +0530

    Clean and update ci.yml and toolchain files

--- cmake/ARMv7ToolChain.cmake
+++ cmake/ARMv7ToolChain.cmake
@@ -1,13 +1,13 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 # Possibly needed tweak
-#SET(CMAKE_SYSTEM_PROCESSOR aarch64)
+#SET(CMAKE_SYSTEM_PROCESSOR armhf)
 
-SET(CMAKE_C_COMPILER ${COMPILER_ROOT}/aarch64-linux-gnu-gcc-8)
-SET(CMAKE_CXX_COMPILER ${COMPILER_ROOT}/aarch64-linux-gnu-g++-8)
+SET(CMAKE_C_COMPILER ${COMPILER_ROOT}/arm-linux-gnueabihf-gcc-8)
+SET(CMAKE_CXX_COMPILER ${COMPILER_ROOT}/arm-linux-gnueabihf-g++-8)
 
 # Below call is necessary to avoid non-RT problem.
-SET(CMAKE_LIBRARY_ARCHITECTURE aarch64-linux-gnu)
-SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE aarch64)
+SET(CMAKE_LIBRARY_ARCHITECTURE arm-linux-gnueabihf)
+SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE armhf)
 
 SET(RASPBERRY_ROOT_PATH ${CMAKE_SOURCE_DIR}/arm_raspberry)
 SET(RASPBERRY_KINETIC_PATH ${RASPBERRY_ROOT_PATH}/opt/ros/kinetic)
@@ -25,11 +25,11 @@ SET(COMPILER_ROOT /usr/bin)
 #SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 INCLUDE_DIRECTORIES(
-	/usr/include/aarch64-linux-gnu)
+	/usr/include/arm-linux-gnueabihf)
 
-SET(ZLIB_LIBRARY /lib/aarch64-linux-gnu/libz.so.1.2.11)
-SET(OPENSSL_CRYPTO_LIBRARY /usr/lib/aarch64-linux-gnu/libcrypto.so.1.1)
-SET(OPENSSL_SSL_LIBRARY /usr/lib/aarch64-linux-gnu/libssl.so.1.1)
+SET(ZLIB_LIBRARY /lib/arm-linux-gnueabihf/libz.so.1.2.11)
+SET(OPENSSL_CRYPTO_LIBRARY /usr/lib/arm-linux-gnueabihf/libcrypto.so.1.1)
+SET(OPENSSL_SSL_LIBRARY /usr/lib/arm-linux-gnueabihf/libssl.so.1.1)
 
 SET(CMAKE_PREFIX_PATH ${RASPBERRY_KINETIC_PATH} ${RASPBERRY_ROOT_PATH}/usr)
 
@@ -40,11 +40,11 @@ SET(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} --sysroot=${RASPBERRY_ROOT_PAT
 
 SET(LD_LIBRARY_PATH ${RASPBERRY_KINETIC_PATH}/lib)
 
-EXECUTE_PROCESS(COMMAND printf "deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ focal main multiverse restricted universe\ndeb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ focal main multiverse restricted universe\ndeb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ focal-updates main multiverse restricted universe\ndeb [arch=amd64] http://archive.ubuntu.com/ubuntu/ focal-updates main multiverse restricted universe\ndeb [arch=amd64] http://security.ubuntu.com/ubuntu/ focal-security main multiverse restricted universe" 
+EXECUTE_PROCESS(COMMAND printf "deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ focal main multiverse restricted universe\ndeb [arch=armhf] http://ports.ubuntu.com/ubuntu-ports/ focal main multiverse restricted universe\ndeb [arch=armhf] http://ports.ubuntu.com/ubuntu-ports/ focal-updates main multiverse restricted universe\ndeb [arch=amd64] http://archive.ubuntu.com/ubuntu/ focal-updates main multiverse restricted universe\ndeb [arch=amd64] http://security.ubuntu.com/ubuntu/ focal-security main multiverse restricted universe" 
 	OUTPUT_FILE TMPFILE)
 EXECUTE_PROCESS(COMMAND sudo mv TMPFILE /etc/apt/sources.list)
-EXECUTE_PROCESS(COMMAND sudo dpkg --add-architecture arm64)
+EXECUTE_PROCESS(COMMAND sudo dpkg --add-architecture armhf)
 EXECUTE_PROCESS(COMMAND sudo apt update)
-EXECUTE_PROCESS(COMMAND sudo apt install -y cmake ninja-build gcc-8-aarch64-linux-gnu g++-8-aarch64-linux-gnu zlib1g-dev:arm64 libssl-dev:arm64 libopus-dev:arm64 libsodium-dev:arm64)
-EXECUTE_PROCESS(COMMAND sudo mv /usr/lib/aarch64-linux-gnu/pkgconfig/libsodium.pc /usr/lib/pkgconfig/)
+EXECUTE_PROCESS(COMMAND sudo apt install -y cmake ninja-build gcc-8-arm-linux-gnueabihf g++-8-arm-linux-gnueabihf zlib1g-dev:armhf libssl-dev:armhf libopus-dev:armhf libsodium-dev:armhf)
+EXECUTE_PROCESS(COMMAND sudo mv /usr/lib/arm-linux-gnueabihf/pkgconfig/libsodium.pc /usr/lib/pkgconfig/)
 
