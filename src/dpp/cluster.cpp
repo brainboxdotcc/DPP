b04568c26bbf3b7692fd1244a5e6993387ab0a41
Axyte <rag21082004+GitHub@gmail.com>
2021-10-03 18:06:12 +0530

    Url encode query parameter

--- src/dpp/cluster.cpp
+++ src/dpp/cluster.cpp
@@ -1083,7 +1083,7 @@ void cluster::guild_get_members(snowflake guild_id, uint16_t limit, snowflake af
 	});
 }
 void cluster::guild_search_members(snowflake guild_id, const std::string& query, uint16_t limit, command_completion_event_t callback) {
-	this->post_rest(API_PATH "/guilds", std::to_string(guild_id), fmt::format("members/search?query=\"{}\"&limit={}", query, limit), m_get, "", [callback, guild_id] (json &j, const http_request_completion_t& http) {
+	this->post_rest(API_PATH "/guilds", std::to_string(guild_id), fmt::format("members/search?query=\"{}\"&limit={}", dpp::url_encode(query), limit), m_get, "", [callback, guild_id] (json &j, const http_request_completion_t& http) {
 		guild_member_map guild_members;
 		for (auto & curr_member : j) {
 			guild_member gm;
