b9be7147af1a1653685b7fb36483496675a691af
brain <craigedwards@brainbox.cc>
2021-10-05 18:59:22 +0100

    automatic commit

---
+++ docpages/makedocs.php
@@ -0,0 +1,18 @@
+<?php
+
+$opts = shell_exec("git tag | perl -p -e s/^v//g | sort -n -r | (echo \"<option value='/'>master</option>\" && sed \"s/.*/<option value='\/&\/'>&<\/option>/\")");
+$opts = preg_replace("/\r|\n|/", "", $opts);
+
+$template = file_get_contents("header.template.html");
+$header = str_replace("##VERSION_OPTIONS##", $opts, $template);
+
+file_put_contents("header.html", $header);
+
+chdir("..");
+system("/usr/local/bin/doxygen");
+system("cp -r docs/* ~/dpp-web/");
+system("cd ~/dpp-web");
+system("git add -A");
+system("git commit -a -m \"automatic commit\"");
+system("git push");
+
