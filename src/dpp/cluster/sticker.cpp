738bd2be35131a536be74dc1951f1d1266d98ae0
brain <craigedwards@brainbox.cc>
2021-10-25 13:23:45 +0100

    build: splitting up of cluster into multiple files each with a subsystem within

---
+++ src/dpp/cluster/sticker.cpp
@@ -0,0 +1,97 @@
+/************************************************************************************
+ *
+ * D++, A Lightweight C++ library for Discord
+ *
+ * Copyright 2021 Craig Edwards and D++ contributors
+ * (https://github.com/brainboxdotcc/DPP/graphs/contributors)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ************************************************************************************/
+#include <dpp/discord.h>
+#include <dpp/cluster.h>
+#include <dpp/nlohmann/json.hpp>
+
+namespace dpp {
+
+void cluster::guild_sticker_create(sticker &s, command_completion_event_t callback) {
+	this->post_rest(API_PATH "/guilds", std::to_string(s.guild_id), "stickers", m_post, s.build_json(false), [callback](json &j, const http_request_completion_t& http) {
+		if (callback) {
+			callback(confirmation_callback_t("sticker", sticker().fill_from_json(&j), http));
+		}
+	}, s.filename, s.filecontent);
+}
+
+
+void cluster::guild_sticker_delete(snowflake sticker_id, snowflake guild_id, command_completion_event_t callback) {
+	this->post_rest(API_PATH "/guilds", std::to_string(guild_id), "stickers/" + std::to_string(sticker_id), m_delete, "", [callback](json &j, const http_request_completion_t& http) {
+		if (callback) {
+			callback(confirmation_callback_t("confirmation", confirmation(), http));
+		}
+	});
+}
+
+
+void cluster::guild_sticker_get(snowflake id, snowflake guild_id, command_completion_event_t callback) {
+	this->post_rest(API_PATH "/guilds", std::to_string(guild_id), "stickers/" + std::to_string(id), m_get, "", [callback](json &j, const http_request_completion_t& http) {
+		if (callback) {
+			callback(confirmation_callback_t("sticker", sticker().fill_from_json(&j), http));
+		}
+	});
+}
+
+
+void cluster::guild_sticker_modify(sticker &s, command_completion_event_t callback) {
+	this->post_rest(API_PATH "/guilds", std::to_string(s.guild_id), "stickers/" + std::to_string(s.id), m_patch, s.build_json(true), [callback](json &j, const http_request_completion_t& http) {
+		if (callback) {
+			callback(confirmation_callback_t("sticker", sticker().fill_from_json(&j), http));
+		}
+	});
+}
+
+
+void cluster::guild_stickers_get(snowflake guild_id, command_completion_event_t callback) {
+	this->post_rest(API_PATH "/guilds", std::to_string(guild_id), "stickers", m_get, "", [callback](json &j, const http_request_completion_t& http) {
+		sticker_map stickers;
+		for (auto & curr_sticker : j) {
+			stickers[SnowflakeNotNull(&curr_sticker, "id")] = sticker().fill_from_json(&curr_sticker);
+		}
+		if (callback) {
+			callback(confirmation_callback_t("sticker_map", stickers, http));
+		}
+	});
+}
+
+
+void cluster::nitro_sticker_get(snowflake id, command_completion_event_t callback) {
+	this->post_rest(API_PATH "/stickers", std::to_string(id), "", m_get, "", [callback](json &j, const http_request_completion_t& http) {
+		if (callback) {
+			callback(confirmation_callback_t("sticker", sticker().fill_from_json(&j), http));
+		}
+	});
+}
+
+
+void cluster::sticker_packs_get(command_completion_event_t callback) {
+	this->post_rest(API_PATH "/sticker-packs", "", "", m_get, "", [callback](json &j, const http_request_completion_t& http) {
+		sticker_pack_map stickerpacks;
+		for (auto & curr_stickerpack : j) {
+			stickerpacks[SnowflakeNotNull(&curr_stickerpack, "id")] = sticker_pack().fill_from_json(&curr_stickerpack);
+		}
+		if (callback) {
+			callback(confirmation_callback_t("sticker_pack_map", stickerpacks, http));
+		}
+	});
+}
+
+};
---
+++ src/dpp/cluster/template.cpp
@@ -0,0 +1,101 @@
+/************************************************************************************
+ *
+ * D++, A Lightweight C++ library for Discord
+ *
+ * Copyright 2021 Craig Edwards and D++ contributors
+ * (https://github.com/brainboxdotcc/DPP/graphs/contributors)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ************************************************************************************/
+#include <dpp/discord.h>
+#include <dpp/cluster.h>
+#include <dpp/nlohmann/json.hpp>
+
+namespace dpp {
+
+void cluster::guild_create_from_template(const std::string &code, const std::string &name, command_completion_event_t callback) {
+	json params;
+	params["name"] = name;
+	this->post_rest(API_PATH "/guilds", "templates", code, m_post, params.dump(), [callback](json &j, const http_request_completion_t& http) {
+		if (callback) {
+			callback(confirmation_callback_t("guild", guild().fill_from_json(nullptr, &j), http));
+		}
+	});
+}
+
+
+void cluster::guild_template_create(snowflake guild_id, const std::string &name, const std::string &description, command_completion_event_t callback) {
+	json params;
+	params["name"] = name;
+	params["description"] = description;
+	this->post_rest(API_PATH "/guilds", std::to_string(guild_id), "templates", m_post, params.dump(), [callback](json &j, const http_request_completion_t& http) {
+		if (callback) {
+			callback(confirmation_callback_t("dtemplate", dtemplate().fill_from_json(&j), http));
+		}
+	});
+}
+
+
+void cluster::guild_template_delete(snowflake guild_id, const std::string &code, command_completion_event_t callback) {
+	this->post_rest(API_PATH "/guilds", std::to_string(guild_id), "templates/" + code, m_delete, "", [callback](json &j, const http_request_completion_t& http) {
+		if (callback) {
+			callback(confirmation_callback_t("dtemplate", dtemplate().fill_from_json(&j), http));
+		}
+	});
+}
+
+
+void cluster::guild_template_modify(snowflake guild_id, const std::string &code, const std::string &name, const std::string &description, command_completion_event_t callback) {
+	json params;
+	params["name"] = name;
+	params["description"] = description;
+	this->post_rest(API_PATH "/guilds", std::to_string(guild_id), "templates/" + code, m_patch, params.dump(), [callback](json &j, const http_request_completion_t& http) {
+		if (callback) {
+			callback(confirmation_callback_t("dtemplate", dtemplate().fill_from_json(&j), http));
+		}
+	});
+}
+
+
+void cluster::guild_templates_get(snowflake guild_id, command_completion_event_t callback) {
+	this->post_rest(API_PATH "/guilds", std::to_string(guild_id), "templates", m_get, "", [callback](json &j, const http_request_completion_t& http) {
+		dtemplate_map dtemplates;
+		for (auto & curr_dtemplate : j) {
+			dtemplates[SnowflakeNotNull(&curr_dtemplate, "id")] = dtemplate().fill_from_json(&curr_dtemplate);
+		}
+		if (callback) {
+				callback(confirmation_callback_t("dtemplate_map", dtemplates, http));
+		}
+	});
+}
+
+
+void cluster::guild_template_sync(snowflake guild_id, const std::string &code, command_completion_event_t callback) {
+	this->post_rest(API_PATH "/guilds", std::to_string(guild_id), "templates/" + code, m_put, "", [callback](json &j, const http_request_completion_t& http) {
+		if (callback) {
+			callback(confirmation_callback_t("dtemplate", dtemplate().fill_from_json(&j), http));
+		}
+	});
+}
+
+
+void cluster::template_get(const std::string &code, command_completion_event_t callback) {
+	this->post_rest(API_PATH "/guilds", "templates", code, m_get, "", [callback](json &j, const http_request_completion_t& http) {
+		if (callback) {
+			callback(confirmation_callback_t("dtemplate", dtemplate().fill_from_json(&j), http));
+		}
+	});
+}
+
+};
