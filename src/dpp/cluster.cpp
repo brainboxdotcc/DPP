78576f6cf13a2ea658aff217960e05eaa2cc17ea
Craig Edwards <braindigitalis@users.noreply.github.com>
2022-01-23 16:34:04 +0000

    Revert "feat: add multiple attachments"

--- src/dpp/cluster.cpp
+++ src/dpp/cluster.cpp
@@ -206,27 +206,6 @@ void cluster::post_rest(const std::string &endpoint, const std::string &major_pa
 	}, postdata, method, get_audit_reason(), filename, filecontent));
 }
 
-void cluster::post_rest_multipart(const std::string &endpoint, const std::string &major_parameters, const std::string &parameters, http_method method, const std::string &postdata, json_encode_t callback, const std::vector<std::string> &filename, const std::vector<std::string> &filecontent) {
-	/* NOTE: This is not a memory leak! The request_queue will free the http_request once it reaches the end of its lifecycle */
-	rest->post_request(new http_request(endpoint + "/" + major_parameters, parameters, [endpoint, callback, this](const http_request_completion_t& rv) {
-		json j;
-		if (rv.error == h_success && !rv.body.empty()) {
-			try {
-				j = json::parse(rv.body);
-			}
-			catch (const std::exception &e) {
-				/* TODO: Do something clever to handle malformed JSON */
-				log(ll_error, fmt::format("post_rest_multipart() to {}: {}", endpoint, e.what()));
-				return;
-			}
-		}
-		if (callback) {
-			callback(j, rv);
-		}
-	}, postdata, method, get_audit_reason(), filename, filecontent));
-}
-
-
 void cluster::request(const std::string &url, http_method method, http_completion_event callback, const std::string &postdata, const std::string &mimetype, const std::multimap<std::string, std::string> &headers) {
 	/* NOTE: This is not a memory leak! The request_queue will free the http_request once it reaches the end of its lifecycle */
 	raw_rest->post_request(new http_request(url, callback, method, postdata, mimetype, headers));
