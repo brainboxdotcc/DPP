47e741f72cae817849935cf65b7e127ab3a3d44b
brain <craigedwards@brainbox.cc>
2022-01-24 00:40:54 +0000

    reintroduce wykert/DPP PR #235

--- src/dpp/message.cpp
+++ src/dpp/message.cpp
@@ -384,13 +384,27 @@ message& message::set_type(message_type t)
 
 message& message::set_filename(const std::string &fn)
 {
-	filename = fn;
+	if (filename.empty()) {
+		filename.push_back(fn);
+	} else {
+		filename[filename.size() - 1] = fn;
+	}
 	return *this;
 }
 
 message& message::set_file_content(const std::string &fc)
 {
-	filecontent = fc;
+	if (filecontent.empty()) {
+		filecontent.push_back(fc);
+	} else {
+		filecontent[filecontent.size() - 1] = fc;
+	}
+	return *this;
+}
+
+message& message::add_file(const std::string &fn, const std::string &fc) {
+	filecontent.push_back(fc);
+	filename.push_back(fn);
 	return *this;
 }
 
@@ -560,16 +574,16 @@ embed& embed::set_url(const std::string &u) {
 
 embed_footer& embed_footer::set_text(const std::string& t){
 	text = t; 
-	return *this;     
+	return *this;
 }
 
-embed_footer& embed_footer::set_icon(const std::string& i){     
+embed_footer& embed_footer::set_icon(const std::string& i){	 
 	icon_url = i;
-	return *this;           
-}                                                                                  
+	return *this;
+}																				  
 
-embed_footer& embed_footer::set_proxy(const std::string& p){     
-	proxy_url = p;          
+embed_footer& embed_footer::set_proxy(const std::string& p){	 
+	proxy_url = p;		  
 	return *this;
 }
 
