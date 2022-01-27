1e7494c5348bf1ea889a5bfab76f8610759b7624
Phil B <info@commandserver.org>
2022-01-27 17:33:57 +0100

    fix: role_edit_position not working

--- src/dpp/cluster/role.cpp
+++ src/dpp/cluster/role.cpp
@@ -58,10 +58,21 @@ void cluster::role_edit(const class role &r, command_completion_event_t callback
 
 
 void cluster::role_edit_position(const class role &r, command_completion_event_t callback) {
-	json j({ {"id", r.id}, {"position", r.position}  });
-	this->post_rest(API_PATH "/guilds", std::to_string(r.guild_id), "roles/" + std::to_string(r.id), m_patch, j.dump(), [r, callback](json &j, const http_request_completion_t& http) {
+	json j({ { {"id", r.id}, {"position", r.position} } } );
+	std::cout << j << std::endl;
+	this->post_rest(API_PATH "/guilds", std::to_string(r.guild_id), "roles", m_patch, j.dump(), [r, callback](json &j, const http_request_completion_t& http) {
 		if (callback) {
-			callback(confirmation_callback_t("role", role().fill_from_json(r.guild_id, &j), http));
+			role rr;
+			confirmation_callback_t e("confirmation", confirmation(), http);
+			if (!e.is_error()) {
+				for (auto &curr_role: j) {
+					if (snowflake_not_null(&curr_role, "id") == r.id) {
+						rr = role().fill_from_json(r.guild_id, &curr_role);
+						break;
+					}
+				}
+			}
+			callback(confirmation_callback_t("role", rr, http));
 		}
 	});
 }
