4b02a95fdd8a769baef608d8499a5a862530298f
Phil B <info@commandserver.org>
2022-01-30 14:11:09 +0100

    updated headers in presence.cpp

--- src/dpp/presence.cpp
+++ src/dpp/presence.cpp
@@ -20,6 +20,8 @@
  ************************************************************************************/
 #include <dpp/presence.h>
 #include <dpp/discordevents.h>
+#include <dpp/utility.h>
+#include <dpp/emoji.h>
 #include <dpp/nlohmann/json.hpp>
 #include <dpp/fmt/format.h>
 
