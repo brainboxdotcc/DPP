d3850691ac3badb21a6765dd9cd3ddc6387990bc
Phil B <info@commandserver.org>
2022-02-07 18:46:44 +0100

    fixed communication_disabled_until parsing in guild_member::build_json

--- src/dpp/guild.cpp
+++ src/dpp/guild.cpp
@@ -180,14 +180,16 @@ bool guild_member::has_animated_guild_avatar() const {
 
 std::string guild_member::build_json() const {
 	json j;
-	if (this->communication_disabled_until == 0) {
-		j["communication_disabled_until"] = NULL;
-	} else {
-		j["communication_disabled_until"] = ts_to_string(this->communication_disabled_until);
+	if (this->communication_disabled_until > 0) {
+		if (this->communication_disabled_until > std::time(nullptr)) {
+			j["communication_disabled_until"] = ts_to_string(this->communication_disabled_until);
+		} else {
+			j["communication_disabled_until"] = json::value_t::null;
+		}
 	}
 	if (!this->nickname.empty())
 		j["nick"] = this->nickname;
-	if (this->roles.size()) {
+	if (!this->roles.empty()) {
 		j["roles"] = {};
 		for (auto & role : roles) {
 			j["roles"].push_back(std::to_string(role));
