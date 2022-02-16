2d77081e380fd7a52e64e09b01667bf9d80218c2
es183923 <72744903+es183923@users.noreply.github.com>
2022-02-15 19:54:58 -0500

    change to break bc it was a bug

--- src/dpp/slashcommand.cpp
+++ src/dpp/slashcommand.cpp
@@ -376,7 +376,7 @@ void from_json(const nlohmann::json& j, command_data_option& cdo) {
 				break;
 			case co_attachment:
 				cdo.value = snowflake_not_null(&j, "value");
-				[[fallthrough]];
+				break;
 			case co_sub_command:
 			case co_sub_command_group:
 			break;
