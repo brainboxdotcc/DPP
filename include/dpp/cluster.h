e7021fca59755514c7963c9620967f144c6076d2
brain <craigedwards@brainbox.cc>
2022-04-15 23:46:30 +0100

    some symbol hacks to make the lib not link properly if the user tries to use debug lib on release on win32 etc

--- include/dpp/cluster.h
+++ include/dpp/cluster.h
@@ -48,6 +48,29 @@ using  json = nlohmann::json;
 
 namespace dpp {
 
+#ifdef _WIN32
+	#ifdef _DEBUG
+		extern "C" void you_are_using_a_release_build_of_dpp_on_a_debug_project();
+	#else
+		extern "C" void you_are_using_a_debug_build_of_dpp_on_a_release_project();
+	#endif
+#endif
+
+struct DPP_EXPORT version_checker {
+	version_checker() {
+		#ifdef _WIN32
+			#ifdef _DEBUG
+				you_are_using_a_release_build_of_dpp_on_a_debug_project()
+			#else
+				you_are_using_a_debug_build_of_dpp_on_a_release_project()
+			#endif
+		#endif
+	}
+};
+
+static version_checker dpp_vc;
+
+
 /**
  * @brief A list of shards
  */
