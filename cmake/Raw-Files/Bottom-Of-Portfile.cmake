09e8c0c2123c56db915aef66d8cd593ca54c0731
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-22 08:20:55 -0400

    Updates!

---
+++ cmake/Raw-Files/Bottom-Of-Portfile.cmake
@@ -0,0 +1,22 @@
+	HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(NO_PREFIX_CORRECTION)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+file(
+	INSTALL "${SOURCE_PATH}/License.md"
+	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+	RENAME copyright
+)
---
+++ cmake/Raw-Files/Top-Of-Portfile.cmake
@@ -0,0 +1,8 @@
+if(VCPKG_TARGET_IS_LINUX)
+    message(WARNING "DiscordCoreAPI only supports g++ 11 on linux.")
+endif()
+
+vcpkg_from_github(
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO RealTimeChris/DiscordCoreAPI
+	REF 
