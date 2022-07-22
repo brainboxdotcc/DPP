38a7faa73f572ddecab4656ef26d8e2784ed7308
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-22 08:24:05 -0400

    Updates!

--- cmake/Raw-Files/Bottom-Of-Portfile.cmake
+++ cmake/Raw-Files/Bottom-Of-Portfile.cmake
@@ -1,4 +1,4 @@
-	HEAD_REF main
+	HEAD_REF master
 )
 
 vcpkg_cmake_configure(
@@ -9,14 +9,15 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(NO_PREFIX_CORRECTION)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share/dpp")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
 file(
-	INSTALL "${SOURCE_PATH}/License.md"
+	INSTALL "${SOURCE_PATH}/LICENSE"
 	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
 	RENAME copyright
 )
