82bd90c54f263432a3115b7adb910e56bfe4b07f
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-07-23 01:45:38 -0400

    Update permissions.h

--- include/dpp/permissions.h
+++ include/dpp/permissions.h
@@ -132,7 +132,7 @@ public:
 	 */
 	template <typename... T>
 	bool has(T... values) const {
-		return (value & (0 | ... | values)) == (0 | ... | values);
+		return (value & (0 | values...)) == (0 | values...);
 	}
 
 	/**
@@ -152,7 +152,7 @@ public:
 	template <typename... T>
 	typename std::enable_if<(std::is_convertible<T, uint64_t>::value && ...), permission&>::type
 	add(T... values) {
-		value |= (0 | ... | values);
+		value |= (0 | values...);
 		return *this;
 	}
 
@@ -172,7 +172,7 @@ public:
 	template <typename... T>
 	typename std::enable_if<(std::is_convertible<T, uint64_t>::value && ...), permission&>::type
 	set(T... values) {
-		value = (0 | ... | values);
+		value = (0 | values...);
 		return *this;
 	}
 
@@ -193,7 +193,7 @@ public:
 	template <typename... T>
 	typename std::enable_if<(std::is_convertible<T, uint64_t>::value && ...), permission&>::type
 	remove(T... values) {
-		value &= ~(0 | ... | values);
+		value &= ~(0 | values...);
 		return *this;
 	}
 };
