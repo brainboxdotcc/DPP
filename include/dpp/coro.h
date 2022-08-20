573d419739d75a30718af918c9c7426b4ea5b376
brain <craigedwards@brainbox.cc>
2022-08-20 14:30:18 +0000

    docs: placeholders for coro docs

--- include/dpp/coro.h
+++ include/dpp/coro.h
@@ -4,43 +4,151 @@
 #include <dpp/restresults.h>
 
 namespace dpp {
+	
+	/**
+	 * @brief 
+	 */
 	using handle_type = std::coroutine_handle<struct promise>;
 
 	class cluster;
 
+	/**
+	 * @brief 
+	 */
 	struct task {
+		/**
+		 * @brief 
+		 */
 		using promise_type = dpp::promise;
 	};
 
+	/**
+	 * @brief 
+	 */
 	struct promise {
+		/**
+		 * @brief 
+		 */
 		cluster* bot = nullptr;
+
+		/**
+		 * @brief 
+		 */
 		confirmation_callback_t callback;
 
+		/**
+		 * @brief Construct a new promise object
+		 */
 		promise() = default;
+
+		/**
+		 * @brief Construct a new promise object
+		 * 
+		 * @param ev 
+		 */
 		promise(const dpp::event_dispatch_t& ev) : bot(ev.from->creator) { }
-		auto get_return_object() { return task{}; }  // { return coroutine::from_promise(*this); }
-		auto initial_suspend() noexcept { return std::suspend_never{}; }
-		auto final_suspend() noexcept { return std::suspend_never{}; }
+
+		/**
+		 * @brief Get the return object object
+		 * 
+		 * @return auto 
+		 */
+		auto get_return_object() {
+			return task{};
+		}
+
+		/**
+		 * @brief 
+		 * 
+		 * @return auto 
+		 */
+		auto initial_suspend() noexcept {
+			return std::suspend_never{};
+		}
+
+		/**
+		 * @brief 
+		 * 
+		 * @return auto 
+		 */
+		auto final_suspend() noexcept {
+			return std::suspend_never{};
+		}
+
+		/**
+		 * @brief 
+		 */
 		void return_void() noexcept {}
+
+		/**
+		 * @brief 
+		 */
 		void unhandled_exception() { 
 			/* try { std::rethrow_exception(std::current_exception()); } */ 
 			/* catch (const std::exception& e) { std::cout << e.what() << '\n'; } */ 
 		}
 	};
 
+	/**
+	 * @brief 
+	 * 
+	 * @tparam T 
+	 */
 	template <typename T> 
 	struct awaitable {
+		/**
+		 * @brief 
+		 */
 		promise* p;
+		
+		/**
+		 * @brief 
+		 * 
+		 */
 		cluster* bot;
+
+		/**
+		 * @brief 
+		 */
 		T api_req;
+
+		/**
+		 * @brief Construct a new awaitable object
+		 * 
+		 * @param cl 
+		 * @param api_call 
+		 */
 		awaitable(cluster* cl, T api_call) : bot{cl}, api_req{api_call} {} 
-		auto await_ready() noexcept { return false; }
+
+		/**
+		 * @brief 
+		 * 
+		 * @return auto 
+		 */
+		auto await_ready() noexcept {
+			return false;
+		}
+
+		/**
+		 * @brief 
+		 * 
+		 * @param handle 
+		 * @return auto 
+		 */
 		auto await_suspend(handle_type handle) { 
 			p = &handle.promise();
 			if (!p->bot) p->bot = bot;
 			api_req([handle](const confirmation_callback_t& cback) { handle.promise().callback = cback; handle.resume(); });
 		}
-		auto await_resume() { return p->callback; }
+
+		/**
+		 * @brief 
+		 * 
+		 * @return auto 
+		 */
+		auto await_resume() {
+			return p->callback;
+		}
 	};
 
 };
