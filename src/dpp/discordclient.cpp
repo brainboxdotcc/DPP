73dddf20e41c395ae5b5a037d583bb7d21376f85
brain <craigedwards@brainbox.cc>
2022-08-24 16:53:14 +0000

    fix: large bot sharding multi-connect spams console with connect message

--- src/dpp/discordclient.cpp
+++ src/dpp/discordclient.cpp
@@ -320,9 +320,9 @@ bool discord_client::handle_frame(const std::string &buffer)
 					resumes++;
 				} else {
 					/* Full connect */
+					log(dpp::ll_debug, "Waiting " + std::to_string(wait) +" seconds before identifying for session...");
 					while (time(nullptr) < creator->last_identify + 5) {
 						time_t wait = (creator->last_identify + 5) - time(nullptr);
-						log(dpp::ll_debug, "Waiting " + std::to_string(wait) +" seconds before identifying for session...");
 						std::this_thread::sleep_for(std::chrono::seconds(wait));
 					}
 					log(dpp::ll_debug, "Connecting new session...");
