2665cba632a920fb2a64830a5b2944522b5d3730
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-08-29 02:22:48 -0400

    Update cluster.cpp

--- src/dpp/cluster.cpp
+++ src/dpp/cluster.cpp
@@ -74,7 +74,7 @@ template<typename T> std::function<void(const T&)> make_intent_warning(cluster* 
 
 cluster::cluster(const std::string &_token, uint32_t _intents, uint32_t _shards, uint32_t _cluster_id, uint32_t _maxclusters, bool comp, cache_policy_t policy, uint32_t request_threads, uint32_t request_threads_raw)
 	: rest(nullptr), raw_rest(nullptr), compressed(comp), start_time(0), token(_token), last_identify(time(NULL) - 5), intents(_intents),
-	numshards(_shards), cluster_id(_cluster_id), maxclusters(_maxclusters), rest_ping(0.0), cache_policy(policy), ws_mode(ws_json)
+	numshards(_shards), cluster_id(_cluster_id), maxclusters(_maxclusters), rest_ping(0.0), cache_policy(policy), ws_mode(ws_json), default_gateway("gateway.discord.gg")
 	
 {
 	/* Instantiate REST request queues */
