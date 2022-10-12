f4da5bedc56502a381d66533850603aba806e925
Craig Edwards <craigedwards@brainbox.cc>
2022-10-12 17:14:31 +0000

    fix: didnt get added

---
+++ src/dpp/event_router.cpp
@@ -0,0 +1,33 @@
+/************************************************************************************
+ *
+ * D++, A Lightweight C++ library for Discord
+ *
+ * Copyright 2021 Craig Edwards and D++ contributors
+ * (https://github.com/brainboxdotcc/DPP/graphs/contributors)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ************************************************************************************/
+#include <dpp/exception.h>
+#include <dpp/nlohmann/json.hpp>
+#include <dpp/event_router.h>
+
+namespace dpp {
+
+static event_handle next_handle = 1;
+
+event_handle event_handle_allocator_t::get_next_event_handle() {
+	return next_handle++;
+}
+
+};
