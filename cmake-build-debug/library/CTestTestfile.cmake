986c5e962cf5ced5cd314402b4a1d5c1c9712914
ruslan-ilesik <ilesik@lesikr.com>
2022-12-02 09:17:39 -0800

    Mention convertions for role user channel and emoji in dpp::utility, also added static converting functions to this classes

--- cmake-build-debug/library/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ilesik/Desktop/DPP/library
-# Build directory: /home/ilesik/Desktop/DPP/cmake-build-debug/library
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unittests "library/unittest")
-set_tests_properties(unittests PROPERTIES  WORKING_DIRECTORY "/library" _BACKTRACE_TRIPLES "/home/ilesik/Desktop/DPP/library/CMakeLists.txt;275;add_test;/home/ilesik/Desktop/DPP/library/CMakeLists.txt;0;")
