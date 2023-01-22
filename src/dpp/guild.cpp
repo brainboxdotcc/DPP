e228a64e6c7747071d14c460c8c5868d108c9319
Phil B <info@commandserver.org>
2023-01-22 12:49:38 +0100

    fixed guild feature parsing for dpp::g_monetization_enabled because discord changed the name from `MONETIZATION_ENABLED` to `CREATOR_MONETIZABLE_PROVISIONAL`

--- src/dpp/guild.cpp
+++ src/dpp/guild.cpp
@@ -47,7 +47,7 @@ const std::map<std::string, std::variant<dpp::guild_flags, dpp::guild_flags_extr
 	{"WELCOME_SCREEN_ENABLED", dpp::g_welcome_screen_enabled },
 	{"MEMBER_VERIFICATION_GATE_ENABLED", dpp::g_member_verification_gate },
 	{"PREVIEW_ENABLED", dpp::g_preview_enabled },
-	{"MONETIZATION_ENABLED", dpp::g_monetization_enabled },
+	{"CREATOR_MONETIZABLE_PROVISIONAL", dpp::g_monetization_enabled },
 	{"MORE_STICKERS", dpp::g_more_stickers },
 	{"ROLE_ICONS", dpp::g_role_icons },
 	{"SEVEN_DAY_THREAD_ARCHIVE", dpp::g_seven_day_thread_archive },
