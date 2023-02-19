c521cbd907305ef80bf6ad6800fb187572772027
Craig Edwards <craigedwards@brainbox.cc>
2023-02-19 13:41:06 +0000

    fix and silence some clang warnings

--- src/dpp/cluster.cpp
+++ src/dpp/cluster.cpp
@@ -297,7 +297,7 @@ json error_response(const std::string& message, http_request_completion_t& rv)
 
 void cluster::post_rest(const std::string &endpoint, const std::string &major_parameters, const std::string &parameters, http_method method, const std::string &postdata, json_encode_t callback, const std::string &filename, const std::string &filecontent) {
 	/* NOTE: This is not a memory leak! The request_queue will free the http_request once it reaches the end of its lifecycle */
-	rest->post_request(new http_request(endpoint + "/" + major_parameters, parameters, [endpoint, callback, this](http_request_completion_t rv) {
+	rest->post_request(new http_request(endpoint + "/" + major_parameters, parameters, [endpoint, callback](http_request_completion_t rv) {
 		json j;
 		if (rv.error == h_success && !rv.body.empty()) {
 			try {
@@ -315,7 +315,7 @@ void cluster::post_rest(const std::string &endpoint, const std::string &major_pa
 
 void cluster::post_rest_multipart(const std::string &endpoint, const std::string &major_parameters, const std::string &parameters, http_method method, const std::string &postdata, json_encode_t callback, const std::vector<std::string> &filename, const std::vector<std::string> &filecontent) {
 	/* NOTE: This is not a memory leak! The request_queue will free the http_request once it reaches the end of its lifecycle */
-	rest->post_request(new http_request(endpoint + "/" + major_parameters, parameters, [endpoint, callback, this](http_request_completion_t rv) {
+	rest->post_request(new http_request(endpoint + "/" + major_parameters, parameters, [endpoint, callback](http_request_completion_t rv) {
 		json j;
 		if (rv.error == h_success && !rv.body.empty()) {
 			try {
