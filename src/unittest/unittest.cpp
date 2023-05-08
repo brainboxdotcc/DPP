1fd11bdacfb7ccd5aa7e08953855503929270360
Phil B <info@commandserver.org>
2023-05-08 19:12:49 +0200

    test: Added unittests for automod rule creation, fetching and deleting

--- src/unittest/unittest.cpp
+++ src/unittest/unittest.cpp
@@ -125,6 +125,9 @@ std::map<std::string, test_t> tests = {
 	{"USER_GET_CACHED_ABSENT", {tt_online, "cluster::user_get_cached_sync() with not present member", false, false}},
 	{"GET_PARAMETER_WITH_SUBCOMMANDS", {tt_offline, "interaction_create_t::get_parameter() with subcommands", false, false}},
 	{"GET_PARAMETER_WITHOUT_SUBCOMMANDS", {tt_offline, "interaction_create_t::get_parameter() without subcommands", false, false}},
+	{"AUTOMOD_RULE_CREATE", {tt_offline, "cluster::automod_rule_create", false, false}},
+	{"AUTOMOD_RULE_GET", {tt_offline, "cluster::automod_rule_edit", false, false}},
+	{"AUTOMOD_RULE_DELETE", {tt_offline, "cluster::automod_rule_delete", false, false}},
 };
 
 double start = dpp::utility::time_f();
@@ -182,7 +185,7 @@ int test_summary() {
 				passed++;
 			}
 		}
-		std::cout << std::left << std::setw(60) << t.second.description << " " << std::fixed << std::setw(6) << (test_skipped ? "\u001b[33mSKIPPED" : (t.second.executed && t.second.success ? "\u001b[32mPASS" : "\u001b[31mFAIL")) << std::setw(0) << "\u001b[0m\n";
+		std::cout << std::left << std::setw(60) << t.second.description << " " << std::fixed << std::setw(6) << (test_skipped ? "\u001b[33mSKIPPED" : (t.second.executed && t.second.success ? "\u001b[32mPASS" : (!t.second.executed ? "\u001b[31mNOT EXECUTED" : "\u001b[31mFAIL"))) << std::setw(0) << "\u001b[0m\n";
 	}
 	std::cout << "\u001b[37;1m\nExecution finished in " << std::fixed << std::setprecision(3) <<  get_time() << std::setprecision(0) << " seconds.\nFailed: " << failed << " Passed: " << passed << (skipped ? " Skipped: " : "") << (skipped ? std::to_string(skipped) : "") << " Percentage: " << std::setprecision(2) << ((float)(passed) / (float)(passed + failed) * 100.0f) << "%\u001b[0m\n";
 	return failed;
