74badc3f6f0233d88bcc36d16d3058053eadf3b6
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2023-05-19 16:51:53 +0700

    refactor: keep the change

--- src/dpp/guild.cpp
+++ src/dpp/guild.cpp
@@ -229,13 +229,20 @@ std::string guild_member::build_json(bool with_id) const {
 			j["communication_disabled_until"] = json::value_t::null;
 		}
 	}
-	if (!this->nickname.empty())
+
+	if (!this->nickname.empty()) {
 		j["nick"] = this->nickname;
+	} else {
+		j["nick"] = find_user(user_id)->username;
+	}
+
 	if (!this->roles.empty()) {
 		j["roles"] = {};
 		for (auto & role : this->roles) {
 			j["roles"].push_back(std::to_string(role));
 		}
+	} else {
+		j["roles"] = {};
 	}
 
 	if (this->flags & gm_voice_action) {
