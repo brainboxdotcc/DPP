e179555929fa329ebe93930a00c02c0740249544
Craig Edwards <craigedwards@brainbox.cc>
2023-07-12 16:34:25 +0000

    better creation of unicode emojis that will actually work

--- buildtools/emojis.php
+++ buildtools/emojis.php
@@ -4,13 +4,14 @@ echo "-- Autogenrating include/dpp/unicode_emoji.h\n";
 
 $header = "#pragma once\n\nnamespace dpp { namespace unicode_emoji {\n";
 
+/* This JSON is generated originally via the NPM package maintained by Discord themselves at https://www.npmjs.com/package/discord-emoji */
 $emojis = json_decode(file_get_contents("https://raw.githubusercontent.com/ArkinSolomon/discord-emoji-converter/master/emojis.json"));
 if ($emojis) {
     foreach ($emojis as $name=>$code) {
         if (preg_match("/^\d+/", $name)) {
             $name = "_" . $name;
         }
-        $header .= "	constexpr std::string " .$name . "(\"$code\");\n";
+        $header .= "	constexpr const char[] " .$name . " = \"$code\";\n";
     }
     $header .= "}\n};\n";
     file_put_contents("include/dpp/unicode_emoji.h", $header);
