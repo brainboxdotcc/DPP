d4a2ad23e75805517e9e553f4d39a865de965b73
wizard7377 <wizard@LAPTOP-DVMGT98I>
2023-08-11 17:35:11 -0400

    Fixed botched commit

--- library/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wizard/openSource/dpp_tests/cs_test/libs/DPP/library
-# Build directory: /home/wizard/openSource/dpp_tests/cs_test/libs/DPP/library
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unittests "library/unittest")
-set_tests_properties(unittests PROPERTIES  WORKING_DIRECTORY "/library" _BACKTRACE_TRIPLES "/home/wizard/openSource/dpp_tests/cs_test/libs/DPP/library/CMakeLists.txt;288;add_test;/home/wizard/openSource/dpp_tests/cs_test/libs/DPP/library/CMakeLists.txt;0;")
