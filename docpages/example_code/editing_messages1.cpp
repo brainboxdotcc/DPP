6782d472d47080b08bfb8c64721bac1b8586a862
Henonicks <130137602+Henonicks@users.noreply.github.com>
2024-05-13 17:24:06 +0300

    docs: Added an example on how to edit an embed (#1128)

--- docpages/example_code/editing_messages1.cpp
+++ docpages/example_code/editing_messages1.cpp
@@ -1,8 +1,8 @@
 #include <dpp/dpp.h>
 
 int main() {
-	dpp::cluster bot("token", dpp::i_default_intents | dpp::i_message_content);
 	/* the second argument is a bitmask of intents - i_message_content is needed to get messages */
+	dpp::cluster bot("Token", dpp::i_default_intents | dpp::i_message_content);
 
 	bot.on_log(dpp::utility::cout_logger());
 
@@ -13,9 +13,8 @@ int main() {
 		} else if (event.command.get_command_name() == "msg-edit") {
 			const auto content = std::get<std::string>(event.get_parameter("content"));
 
-			/* get message to edit it after */
+			/* get the message to edit it after. here string will automatically be converted to snowflake */
 			const dpp::snowflake msg_id = std::get<std::string>(event.get_parameter("msg-id"));
-			/* here string will automatically be converted to snowflake */
 
 			bot.message_get(msg_id, event.command.channel_id, [&bot, content, event](const dpp::confirmation_callback_t& callback) {
 				if (callback.is_error()) {
@@ -27,51 +26,24 @@ int main() {
 				/* change the message content and edit the message itself */
 				message.set_content(content);
 				bot.message_edit(message);
-
 				event.reply("Message content is now `" + content + "`.");
-			});
-		} else if (event.command.get_command_name() == "channel-edit") {
-			const auto name = std::get<std::string>(event.get_parameter("name"));
-
-			/* get the channel to edit it after */
-			const auto channel_id = std::get<dpp::snowflake>(event.get_parameter("channel"));
-
-			bot.channel_get(channel_id, [&bot, name, event](const dpp::confirmation_callback_t& callback) {
-				if (callback.is_error()) {
-					event.reply("error");
-					return;
-				}
-				auto channel = callback.get<dpp::channel>();
-
-				/* change the channel name and edit the channel itself */
-				channel.set_name(name);
-				bot.channel_edit(channel);
-
-				event.reply("Channel name is now `" + name + "`.");
 			});
 		}
 	});
 
 	bot.on_ready([&bot](const dpp::ready_t& event) {
-
 		if (dpp::run_once <struct register_global_commands>()) {
 			dpp::slashcommand msg_edit("msg-edit", "Edit a message sent by the bot", bot.me.id);
 
 			msg_edit.add_option(dpp::command_option(dpp::co_string, "msg-id", "ID of the message to edit", true)); /* true for required option */
 			msg_edit.add_option(dpp::command_option(dpp::co_string, "content", "New content for the message", true)); /* same here */
 
-			dpp::slashcommand channel_edit("channel-edit", "Edit the name of channel specified", bot.me.id);
-
-			channel_edit.add_option(dpp::command_option(dpp::co_channel, "channel", "Channel to edit", true));
-			channel_edit.add_option(dpp::command_option(dpp::co_string, "name", "New name for the channel", true));
-
 			dpp::slashcommand msg_send("msg-send", "Send my message", bot.me.id);
 
-			bot.global_bulk_command_create({ msg_edit, channel_edit, msg_send });
+			bot.global_bulk_command_create({ msg_edit, msg_send });
 		}
 	});
 
 	bot.start(dpp::st_wait);
-	
 	return 0;
 }
