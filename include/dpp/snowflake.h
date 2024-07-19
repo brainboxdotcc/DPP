fa7a831346a4c7ca94f333bd8c3f680f0f7d65b7
ruslan-ilesik <ruslan@ilesik.com>
2024-07-19 16:04:20 +0200

    cmake check for format

--- include/dpp/snowflake.h
+++ include/dpp/snowflake.h
@@ -26,18 +26,10 @@
 #include <cstdint>
 #include <type_traits>
 
-#if __cplusplus >= 202002L
-//fix for https://github.com/llvm/llvm-project/issues/77773 and
-// apple support on clang 14 https://developer.apple.com/xcode/cpp/
-#if (defined(__cpp_lib_format) || \
-        (defined(__clang__) && __clang_major__ >= 14 && !defined(__APPLE__) && __has_include(<format>))) || ( defined(__GNUC__) &&  __GNUC__ >= 13)
-
-#define DPP_HAS_FORMAT
-
+#ifdef DPP_HAS_FORMAT
 #include <format>
+#endif
 
-#endif
-#endif
 
 /**
  * @brief The main namespace for D++ functions. classes and types
