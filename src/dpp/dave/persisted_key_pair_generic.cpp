4ba4eec659a0a9c6c2a6e638e35af561b511222e
Craig Edwards <craigedwards@brainbox.cc>
2024-09-25 15:59:52 +0000

    work around defect with stringstream inplace

--- src/dpp/dave/persisted_key_pair_generic.cpp
+++ src/dpp/dave/persisted_key_pair_generic.cpp
@@ -92,7 +92,8 @@ std::shared_ptr<::mlspp::SignaturePrivateKey> GetGenericPersistedKeyPair(KeyPair
             return nullptr;
         }
 
-        curstr = (std::stringstream() << ifs.rdbuf()).str();
+	std::stringstream s = (std::stringstream() << ifs.rdbuf());
+	curstr = s.str();
         if (!ifs) {
             DISCORD_LOG(LS_ERROR) << "Failed to read key in GetPersistedKeyPair";
             return nullptr;
