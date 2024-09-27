7e1d3779999d81a9a4a1862f791d395054cf42d3
Craig Edwards <craigedwards@brainbox.cc>
2024-09-27 07:35:17 +0000

    remove mpack.variant, it is only used for C++11 and C++14, and macos from 2015. Not needed in DPP

--- mlspp/lib/bytes/include/tls/compat.h
+++ mlspp/lib/bytes/include/tls/compat.h
@@ -11,14 +11,7 @@
 
 namespace mlspp::tls {
 
-// To balance backward-compatibility with macOS 10.11 with forward-compatibility
-// with future versions of C++, we use `mpark::variant` or `std::variant` as
-// needed, using `var::variant` to refer to whichever one is in use.
-#ifdef VARIANT_COMPAT
-namespace var = mpark;
-#else
 namespace var = std;
-#endif // VARIANT_COMPAT
 
 // In a similar vein, we provide our own safe accessors for std::optional, since
 // std::optional::value() is not available on macOS 10.11.
