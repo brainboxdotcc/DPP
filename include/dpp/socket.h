4e86993087fe35fdad0141a0f54dccd0d14eff7e
Craig Edwards <craigedwards@brainbox.cc>
2024-10-16 01:27:40 +0000

    fix: failed to include export.h

--- include/dpp/socket.h
+++ include/dpp/socket.h
@@ -21,6 +21,7 @@
  ************************************************************************************/
 #pragma once
 
+#include <dpp/export.h>
 #ifdef _WIN32
 	#include <WinSock2.h>
 	#include <WS2tcpip.h>
