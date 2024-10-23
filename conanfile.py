a486bc79f9b44dc3f1f103066f93027695482cc3
Craig Edwards <craigedwards@brainbox.cc>
2024-10-23 22:47:01 +0000

    validator

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,13 @@
 import os
 import shutil
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 from conan.tools.scm import Git
 from conan.tools.files import update_conandata, download, unzip
+
+required_conan_version = ">=2.0"
 
 class DPPConan(ConanFile):
     name = "dpp"
@@ -17,6 +21,29 @@ class DPPConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": True, "fPIC": True}
+
+    @property
+    def _min_cppstd(self):
+        return 17
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "8",
+            "clang": "11",
+            "apple-clang": "14",
+            "Visual Studio": "17",
+            "msvc": "193",
+        }
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
 
     def requirements(self):
         self.requires("nlohmann_json/3.11.2")
