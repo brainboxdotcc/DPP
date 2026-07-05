50f45c507f47ff8fe0a945337f44311aebcb64b9
Neko-Life <nekolife123579@gmail.com>
2026-07-05 21:56:07 +0700

    fix: assign INVALID_SOCKET instead so no socket_events handlers should process the socket anymore

--- src/dpp/discordclient.cpp
+++ src/dpp/discordclient.cpp
@@ -126,7 +126,7 @@ void discord_client::on_disconnect()
 	set_resume_hostname();
 	if (sfd != INVALID_SOCKET) {
 		log(dpp::ll_debug, "Lost connection to websocket on shard " + std::to_string(shard_id) + ", reconnecting...");
-		owner->socketengine->delete_socket(sfd);
+		sfd = INVALID_SOCKET;
 	}
 	ssl_connection::close();
 	owner->stop_timer(timer_handle);
